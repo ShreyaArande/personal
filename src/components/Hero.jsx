0b4864bb43ba87165fff19bd7e1ef76af334a640
otobit-shreya <arshreya@otobit.in>
2023-05-22 15:54:36 +0530

    First Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,68 @@
+import { motion, useAnimation } from "framer-motion";
+import { styles } from "../styles";
+import { useEffect, useState } from "react";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  const controls = useAnimation();
+
+  // Bouncing effect for h1 text
+  useEffect(() => {
+    controls.start({
+      y: [0, -10, 0], // Define the y-axis values for the bouncing effect
+      transition: {
+        duration: 1, // Adjust the duration of the animation (in seconds)
+        repeat: 1, // Set the animation to repeat indefinitely
+      },
+    });
+  }, [controls]);
+
+  // Typing effect for paragraph text
+  const textContent =
+    "Experienced React.js developer delivering captivating, high-quality user interfaces.";
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    const typingAnimation = async () => {
+      for (let i = 0; i <= textContent.length; i++) {
+        setDisplayText(textContent.slice(0, i));
+        await new Promise((resolve) => setTimeout(resolve, 80));
+      }
+    };
+
+    typingAnimation();
+  }, []);
+  return (
+    <>
+      <section className="relative w-full h-screen">
+        <div
+          className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        >
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
+          <div>
+            <motion.p
+              className={`${styles.heroSubText} mt-2 text-white-100`}
+              animate={controls}
+            >
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hi, I am <span className="text-[#915eff]">Shreya</span>
+              </h1>
+            </motion.p>
+
+            <motion.p
+              className={`${styles.heroSubText} mt-2 text-white-100`}
+            >
+              {displayText}
+            </motion.p>
+          </div>
+        </div>
+        <ComputersCanvas />
+      </section>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/no-unknown-property */
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CanvasLoader from "../Loader";
+import React, { Suspense, useEffect, useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        intensity={1}
+        angle={0.12}
+        penumbra={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={1}
+        position={[0, -2.0, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
